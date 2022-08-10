200b397a1d1a072aadfa3c350339e74f6cc027f1
GPC <anthonytguilherme@hotmail.com>
2022-08-10 17:28:43 -0300

    add a functionality for the all buttons of the part to robotron

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-const braco = document.querySelector('#braco')
-const subtrair = document.querySelector('#subtrair')
-const somar = document.querySelector('#somar')
-
 const controle = document.querySelectorAll('.controle-ajuste')
 
+controle.forEach((elemento)=>{
+    elemento.addEventListener('click', (evento)=>{
+       manipulaDados(evento.target.textContent, evento.target.parentNode)
+    })
+})
 
-somar.addEventListener('click', ()=>{manipulaDados('somar')})
 
-subtrair.addEventListener('click', ()=>{manipulaDados('subtrair')})
+function manipulaDados(operacao, controle){
 
-function manipulaDados(operacao){
-    if(operacao === 'subtrair'){
-        braco.value = parseInt(braco.value) -1
+    const peca = controle.querySelector('.controle-contador')
+
+    if(operacao === '-'){
+        peca.value = parseInt(peca.value) - 1
     }else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
 }
+
